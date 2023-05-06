2b13e4ea86659bcb476481ab690966576d8d4151
dani <danifromecuador@gmail.com>
2023-05-06 16:05:52 -0400

    Change Button.PropTypes for Button.propTypes

--- src/components/Button.js
+++ src/components/Button.js
@@ -15,7 +15,7 @@ export default function Button({ character, className, fn }) {
   );
 }
 
-Button.PropTypes = {
+Button.propTypes = {
   character: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
   fn: PropTypes.func.isRequired,
