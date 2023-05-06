42a8b998398e5640e30c26c1284e6a0d9c25ebc4
dani <danifromecuador@gmail.com>
2023-05-06 18:10:22 -0400

    Show a quote from API

---
+++ src/components/Quote.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+
+function Quote() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  const [error, setError] = useState('error');
+  const [loading, setLoading] = useState('not loading');
+  const fetchQuote = async () => {
+    const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+      headers: {
+        'X-Api-Key': 'nezpEokeOVTwP1C5NwtGEQ==h62LjJXfJNexoQfy',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    const { quote, author, category } = data[0];
+    setQuote(quote);
+    setAuthor(author);
+    setCategory(category);
+    setError('not error');
+    setLoading('loading');
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  if (loading === 'not loading') {
+    return (
+      <p className="loadingMessage">
+        Loading...
+      </p>
+    );
+  }
+
+  if (error === 'error') {
+    return (
+      <p className="errorMessage">
+        Error
+      </p>
+    );
+  }
+
+  return (
+    <div className="quoteContainer">
+      <p>{quote}</p>
+      <span>
+        {author}
+        -
+      </span>
+      <span>{category}</span>
+    </div>
+  );
+}
+
+export default Quote;
